f0eff84a2538e9f34f03bcb29df1137a8462b1f6
MartinLucena28 <martinlucena02@gmail.com>
2023-01-12 11:27:30 -0300

    Initial commit

---
+++ src/main/java/com/portfolio/cml/security/Enums/RolNombre.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.portfolio.cml.security.Enums;
+
+/**
+ *
+ * @author pc
+ */
+public enum RolNombre {
+    ROLE_ADMIN, ROLE_USER
+}
