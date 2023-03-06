77b7419611d47f270a4442c36c06a9478bde1f95
MartinLucena28 <martinlucena02@gmail.com>
2023-03-06 16:36:46 -0300

    v3.6

--- src/main/java/com/portfolio/cml/Controller/CEducacion.java
+++ src/main/java/com/portfolio/cml/Controller/CEducacion.java
@@ -28,7 +28,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author pc
  */@RestController
 @RequestMapping("/educacion")
-@CrossOrigin(origins = {"https://frontendcml.web.app","http://localhost:4200"})
+@CrossOrigin(origins = {"*"})
 public class CEducacion {
     @Autowired
     SEducacion sEducacion;
--- src/main/java/com/portfolio/cml/Controller/CExperiencia.java
+++ src/main/java/com/portfolio/cml/Controller/CExperiencia.java
@@ -28,7 +28,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author pc
  */@RestController
 @RequestMapping("/explab")
-@CrossOrigin(origins = {"https://frontendcml.web.app","http://localhost:4200"})
+@CrossOrigin(origins = {"*"})
 public class CExperiencia {
     @Autowired
     SExperiencia sExperiencia;
